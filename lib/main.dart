131ebed0ef12c06d2ba5d8683b186f8de67844ab
OceanAcademy <oceandocuments@gmail.com>
2019-11-30 16:31:05 +0530

    Abstraction and encapsulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
 
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +27,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  bool answerAndNextQuestion() {
+    bool correctAnswer = quizBrain.getAnswer();
+    setState(() {
+      quizBrain.nextQuestion();
+    });
+    return correctAnswer;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +73,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answerAndNextQuestion();
+                if (correctAnswer == true) {
+                  print('you got it right');
+                } else {
+                  print('you got it wrong');
+                }
                 //The user picked true.
               },
             ),
@@ -79,19 +97,21 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = answerAndNextQuestion();
+                if (correctAnswer == false) {
+                  print('you got it right');
+                } else {
+                  print('you got it wrong');
+                }
                 //The user picked false.
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
